a66783434f03ad723218d1607d93ce92b3c4b760
IvanBlacky <tcherivan@gmail.com>
2017-02-03 19:12:57 +0300

    Запоминаются закладки и история

--- server.js
+++ server.js
@@ -5,20 +5,21 @@ var cookieParser = require('cookie-parser');
 
 app.use(bodyParser());
 app.use(bodyParser.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.static(__dirname + '/src'));
 
 
 var nunjucks = require('nunjucks');
 nunjucks.configure(__dirname + '/src', {
-    autoescape: true,
-    cache: false,
-    express: app
+	autoescape: true,
+	cache: false,
+	express: app
 });
 
 
 app.use('/', require('./app/router'));
 
 
-app.listen(require('./app/config.js').port);
+app.listen(require('./app/config.js')
+	.port);
 console.log('Server started!');
