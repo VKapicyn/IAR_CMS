594e8bfdb35b6e7619d7c5b935b84354be701606
IvanBlacky <tcherivan@gmail.com>
2017-02-25 18:41:46 +0300

    HOTUPDATE №1
    Реализация бредкрамбс, нужно для верстки

--- app/controllers/getPage.js
+++ app/controllers/getPage.js
@@ -1,22 +1,45 @@
 var fs = require('fs');
 
 
-exports.page = function(req, res){
+exports.page = function(req, res) {
 	try {
 		if ((req.cookies['lang'] == 'ru') || (req.cookies['lang'] == undefined)) {
 			res.render(req.params.alias + '_ru' + '.html', {
-				title: getTitle(req.params.alias, 'ru')
+				title: getTitle(req.params.alias, 'ru'),
+				breadcrumbs: getBreadCrumbs(req.params.alias)
 			});
-		}
-		else{
+		} else {
 			res.render(req.params.alias + '_en' + '.html', {
 				title: getTitle(req.params.alias, 'en')
 			});
 		}
-	}
-	catch (err) {
+	} catch (err) {
 		res.redirect('/page/404');
 	}
+}
+
+function getParent(alias_to_seek) {
+	var parent;
+	let config = getPagesConfig();
+	config = config.pages;
+
+	for (var page in config) {
+		if (config[page].alias == alias_to_seek)
+			if (config[page].parent) {
+				parent = config[page].parent;
+			}
+	}
+	return parent;
+}
+
+function getBreadCrumbs(alias_to_seek) {
+	var breadcrumbs = Array();
+	breadcrumbs[0] = alias_to_seek;
+
+	while (breadcrumbs[breadcrumbs.length - 1] != 'main') {
+		breadcrumbs.push(getParent(breadcrumbs[breadcrumbs.length - 1]));
+	}
+	return breadcrumbs;
 }
 
 function getTitle(alias, lang) {
